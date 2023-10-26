2d13921c5aa09af30fa3b92a2deb21b3ff7ae233
kundi21 <cundi.machado@gmail.com>
2023-10-26 19:07:22 +0000

    using my branch

--- app/favourites/page.js
+++ app/favourites/page.js
@@ -20,7 +20,9 @@ export default function Home() {
 
     const deleteFavourite = (product_id) => {
         const copy = [...products];
-        const filteredProducts = copy.filter((product) => product._id !== product_id);
+        const filteredProducts = copy.filter((product) => {
+            return product._id !== product_id
+        });
         setProducts(filteredProducts);
         console.log(filteredProducts);
     }
