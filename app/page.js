3e351f7e90b80269490abf2fe497261d76fb0e67
kundi21 <cundi.machado@gmail.com>
2023-11-14 23:51:04 +0000

    Implemented favorite logic for home page

--- app/page.js
+++ app/page.js
@@ -5,26 +5,27 @@ import { useState, useEffect } from "react";
 
 export default function Home() {
   const [products, setProducts] = useState([]);
+  const [favorites, setFavorites] = useState([]);
 
-  const fetchProducts = async () => {
+  const fetchInfo = async () => {
     const res = await fetch("http://localhost:8080/products");
     const data = await res.json();
+
+    const res2 = await fetch(`http://localhost:8080/users/profile`, {
+      method: "GET",
+      headers: { "Content-Type": "application/json" },
+      credentials: "include",
+    });
+
+    const profileData = await res2.json();
     setProducts(data);
+    setFavorites(profileData.favProducts);
+    console.log(profileData.favProducts);
   };
 
   useEffect(() => {
-    fetchProducts();
+    fetchInfo();
   }, []);
-
-  // return (
-  //   <>
-  //     <div className="flex flex-row flex-wrap justify-center">
-  //       {products.map((product) => (
-  //         <Card product={product} key={product._id} />
-  //       ))}
-  //     </div>
-  //   </>
-  // );
 
   return (
     <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
@@ -32,9 +33,7 @@ export default function Home() {
 
       <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
         {products.map((product) => (
-          // TODO: check if product id is in the list of favorites and pass it to the card as a prop
-          // TODO: first we have to fetch the list of favorites from the backend
-          <Card product={product} key={product._id} />
+          <Card product={product} key={product._id} isFavorite={(favorites.includes(product._id))} favorites={favorites} setFavorites={setFavorites} />
         ))}
       </div>
     </div>
